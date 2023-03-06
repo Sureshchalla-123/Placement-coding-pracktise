f2955ab7912c4b0e582a1a1c718d3129d1a76f46
SURESH CHALLA <sureshchalla9876@gmail.com>
2023-03-06 19:03:36 +0530

    update1

---
+++ add_two_numbers.py
@@ -0,0 +1,13 @@
+class Main:
+
+    def addTwoNum(self,A,B):
+
+        return A+B
+
+
+    if __name__=='__main__':
+        obj = Main()
+        a=int(input())
+        b=int(input())
+        res=obj.addTwoNum(a,b)
+        print(res)
